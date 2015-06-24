b9130040104bd5c9045db26dd3885791fa8bd7d8
ih64 <imran.hasan@yale.edu>
2015-06-24 13:51:50 -0400

    align module using alipy to align images from dif dithers

--- irproc.py
+++ irproc.py
@@ -1,8 +1,14 @@
-import glob
+import glob, sys
 import numpy as np 
 from pyraf import iraf
-import pandas as pd 
+import pandas as pd
+#set append the python path so it can use alipy and stuff
+sys.path.append('/home/ih64/python_modules/')
+import alipy
+#do this stuff down here so IRAF doesn't save any parameters, and to make important tasks avaliable 
 iraf.prcacheOff()
+iraf.imred()
+iraf.ccdred()
 
 def makeSkyFlat(dfView):
 	'''
@@ -65,7 +71,7 @@ def flatten(flatFile):
 	skyims=glob.glob('skybinir*fits')
 	outputIms=['sky-flatten-'+i[3:] for i in skyims]
 	#now flatfield the sky subtracted images
-	iraf.ccdproc (','.join(skyims), output=','.join(outimage), 
+	iraf.ccdproc(images=','.join(skyims), output=','.join(outputIms), 
 		ccdtype="", max_cache=0, noproc="no", fixpix="no", overscan="no",
 		trim="no", zerocor="no", darkcor="no", flatcor="yes", illumcor="no", fringecor="no",
 		readcor="no", scancor="no", readaxis="line", fixfile="", biassec="", trimsec="",
@@ -74,3 +80,36 @@ def flatten(flatFile):
 		sample="*", naverage=1, niterate=1, low_reject=3., high_reject=3., grow=0.)
 	return
 
+def align():
+	'''align the flatten images'''
+	images_to_align =sorted(glob.glob("sky-flatten-*fits"))
+	ref_image = images_to_align[0]
+
+	#everything below here i copied from the alipy demo http://obswww.unige.ch/~tewes/alipy/tutorial.html
+	identifications = alipy.ident.run(ref_image, images_to_align, visu=False)
+	# That's it !
+	# Put visu=True to get visualizations in form of png files (nice but much slower)
+	# On multi-extension data, you will want to specify the hdu (see API doc).
+
+	# The output is a list of Identification objects, which contain the transforms :
+	for id in identifications: # list of the same length as images_to_align.
+		if id.ok == True: # i.e., if it worked
+			print "%20s : %20s, flux ratio %.2f" % (id.ukn.name, id.trans, id.medfluxratio)
+			# id.trans is a alipy.star.SimpleTransform object. Instead of printing it out as a string,
+			# you can directly access its parameters :
+			#print id.trans.v # the raw data, [r*cos(theta)  r*sin(theta)  r*shift_x  r*shift_y]
+			#print id.trans.matrixform()
+			#print id.trans.inverse() # this returns a new SimpleTransform object
+		else:
+			print "%20s : no transformation found !" % (id.ukn.name)
+
+	# Minimal example of how to align images :
+	outputshape = alipy.align.shape(ref_image)
+	# This is simply a tuple (width, height)... you could specify any other shape.
+
+	for id in identifications:
+		if id.ok == True:
+			# Variant 2, using geomap/gregister, correcting also for distortions :
+			alipy.align.irafalign(id.ukn.filepath, id.uknmatchstars, id.refmatchstars,verbose=False,
+				shape=outputshape, makepng=False, outdir="./")
+	return
