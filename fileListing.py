c5beacaee350c68f0bd25f216e51f5f62a467b00
ih64 <imran.hasan@yale.edu>
2015-08-07 16:02:36 -0400

    chchecks df entries for nulls, prints error message if it finds some and tells you to fix files

--- fileListing.py
+++ fileListing.py
@@ -28,17 +28,44 @@ def hselToDf(color,frameFlavor='raw'):
 	#stick the file handle into a pandas parser to read it into a data frame
 	fileTable=pd.io.parsers.read_fwf(fileHandle, names=['file','JulianDate'])
 	fileHandle.close()
-	#grab the YYMMDD dates from the file name and list these in a column
 
-	if frameFlavor=='raw':
-		fileTable['Date']=fileTable.file.apply(lambda i: i[-16:-10]).astype(int)
-		#pickle the data frame
-		fileTable.to_pickle(color.upper()+'rawList.pkl')
-	else:
-		fileTable['Date']=fileTable.file.apply(lambda i: i[-17:-11]).astype(int)
-		#pickle the data frame
-		fileTable.to_pickle(color.upper()+'flatList.pkl')	
-	return
+	#Try to force the JulianDate data to floats. This wont work if its corrupted somehow,
+	#or has a bad value printed, like a string.
+	try:
+		fileTable['JulianDate']=fileTable['JulianDate'].astype(float)
+		#grab the YYMMDD dates from the file name and list these in a column
+		if frameFlavor=='raw':
+			fileTable['Date']=fileTable.file.apply(lambda i: i[-16:-10]).astype(int)
+			#check for nulls in the dataframe
+			if checkNullColumn(fileTable):
+				#pickle the data frame
+				fileTable.to_pickle(color.upper()+'rawList.pkl')
+				return
+			else:
+				return
+		else:
+			if checkNullColumn(fileTable):
+				fileTable['Date']=fileTable.file.apply(lambda i: i[-17:-11]).astype(int)
+				#pickle the data frame
+				fileTable.to_pickle(color.upper()+'flatList.pkl')	
+				return
+			else:
+				return
+	except ValueError:
+		print "encountered a value error. There is a bad value in either the JD field of a header, or a nonstandard filename"
+		print "fix these and try again"
+		return
+
+def checkNullColumn(df):
+	columns=df.columns
+	for name in columns:
+		if df[name][pd.isnull(df[name])].size > 0:
+			print "found nulls in the data set"
+			print df.ix[df[name][pd.isnull(df[name])].index]
+			print "fix these and try again"
+			return False
+		else:
+			return True
 
 if __name__ =='__main__':
 	hselToDf(str(sys.argv[1]), frameFlavor=str(sys.argv[2]))
